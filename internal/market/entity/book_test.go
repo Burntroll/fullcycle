e017693655acb57992ae71cb71808c47d2a24e04
Francisco Silva <fco.silva.dev@gmail.com>
2023-06-26 12:14:03 +0300

    Add TestNoMatch

--- internal/market/entity/book_test.go
+++ internal/market/entity/book_test.go
@@ -202,3 +202,41 @@ func TestBuyWithDifferentPrice(t *testing.T) {
 	// assert.Equal(10.0, float64(book.Transactions[1].Total), "Transaction should have price 10")
 }
 
+func TestNoMatch(t *testing.T) {
+	asset1 := NewAsset("asset1", "Asset 1", 100)
+
+	investor := NewInvestor("1")
+	investor2 := NewInvestor("2")
+
+	investorAssetPosition := NewInvestorAssetPosition("asset1", 3)
+	investor.AddAssetPosition(investorAssetPosition)
+
+	wg := sync.WaitGroup{}
+	orderChan := make(chan *Order)
+
+	orderChanOut := make(chan *Order)
+
+	book := NewBook(orderChan, orderChanOut, &wg)
+	go book.Trade()
+
+	wg.Add(0)
+	// investidor 1 wants to sell 3 shares
+	order := NewOrder("1", investor, asset1, 3, 6.0, "SELL")
+	orderChan <- order
+
+	// investidor 2 wants to buy 5 shares
+	order2 := NewOrder("2", investor2, asset1, 5, 5.0, "BUY")
+	orderChan <- order2
+
+	go func() {
+		for range orderChanOut {
+		}
+	}()
+	wg.Wait()
+
+	assert := assert.New(t)
+	assert.Equal("OPEN", order.Status, "Order 1 should be closed")
+	assert.Equal("OPEN", order2.Status, "Order 2 should be OPEN")
+	assert.Equal(3, order.PendingShares, "Order 1 should have 3 PendingShares")
+	assert.Equal(5, order2.PendingShares, "Order 2 should have 5 PendingShares")
+}
