3fcb591206c79357a6767b852a9326370c495d2c
Francisco Silva <fco.silva.dev@gmail.com>
2023-06-27 10:39:05 +0300

    add producer

--- internal/infra/kafka/consumer.go
+++ internal/infra/kafka/consumer.go
@@ -3,14 +3,14 @@ package kafka
 import ckafka "github.com/confluentinc/confluent-kafka-go/kafka"
 
 type Consumer struct {
-	ConfigMap	*ckafka.ConfigMap
-	Topics		[]string
+	ConfigMap *ckafka.ConfigMap
+	Topics    []string
 }
 
 func NewConsumer(configMap *ckafka.ConfigMap, topics []string) *Consumer {
 	return &Consumer{
 		ConfigMap: configMap,
-		Topics: topics,
+		Topics:    topics,
 	}
 }
 
