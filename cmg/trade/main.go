1708ec05ea063a0f8a9564cc01e89765eb324343
Francisco Silva <fco.silva.dev@gmail.com>
2023-06-27 11:07:59 +0300

    Add main

---
+++ cmg/trade/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"sync"
+
+	"github.com/Burntroll/fullcycle/go/internal/infra/kafka"
+	"github.com/Burntroll/fullcycle/go/internal/market/dto"
+	"github.com/Burntroll/fullcycle/go/internal/market/entity"
+	"github.com/Burntroll/fullcycle/go/internal/market/transformer"
+	ckafka "github.com/confluentinc/confluent-kafka-go/kafka"
+)
+
+func main() {
+	ordersIn := make(chan *entity.Order)
+	ordersOut := make(chan *entity.Order)
+	wg := &sync.WaitGroup{}
+	defer wg.Wait()
+
+	kafkaMsgChan := make(chan *ckafka.Message)
+	configMap := &ckafka.ConfigMap{
+		"bootstrap.servers": "host.docker.internal:9094",
+		"group.id":          "myGroup",
+		"auto.offset.reset": "latest",
+	}
+	producer := kafka.NewKafkaProducer(configMap)
+	kafka := kafka.NewConsumer(configMap, []string{"input"})
+
+	go kafka.Consume(kafkaMsgChan) // T2
+
+	//eceive from the kafka channel, throw it in the input, process it, throw it in the output and then publish it in kafka
+	book := entity.NewBook(ordersIn, ordersOut, wg)
+	go book.Trade() // T3
+
+	go func() {
+		for msg := range kafkaMsgChan {
+			wg.Add(1)
+			fmt.Println(string(msg.Value))
+			tradeInput := dto.TradeInput{}
+			err := json.Unmarshal(msg.Value, &tradeInput)
+			if err != nil {
+				panic(err)
+			}
+			order := transformer.TransformInput(tradeInput)
+			ordersIn <- order
+		}
+	}()
+
+	for res := range ordersOut {
+		output := transformer.TransformOutput(res)
+		outputJson, err := json.MarshalIndent(output, "", "  ")
+		fmt.Println(string(outputJson))
+		if err != nil {
+			fmt.Println(err)
+		}
+		producer.Publish(outputJson, []byte("orders"), "output")
+	}
+}
